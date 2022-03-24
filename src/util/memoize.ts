3d9ca45eebfff84214b49a63437508423405c070
eagr <eagr@tutanota.com>
2022-03-24 16:33:29 +0800

    bigint-aware stringify()

--- src/util/memoize.ts
+++ src/util/memoize.ts
@@ -1,9 +1,14 @@
+function stringify (x:any) {
+    if (x === undefined) return
+    return JSON.stringify(x, (_, v) => typeof v === 'bigint' ? v.toString() : v)
+}
+
 function memoize<F extends unknown>(f:F) : F {
     if (typeof f !== 'function') throw new TypeError('Expect a function')
 
     const cache = new Map()
     const memoized = function (...args) {
-        const key = JSON.stringify(args)
+        const key = stringify(args)
         if (cache.has(key)) return cache.get(key)
 
         const res = f.apply(null, args)
