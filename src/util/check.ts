1b4e9740da10b0a798ab1d1b2a8d86007840d4d5
eagr <eagr@tutanota.com>
2022-03-24 18:51:16 +0800

    include bigint checks

--- src/util/check.ts
+++ src/util/check.ts
@@ -1,19 +1,46 @@
+function isBigInt (x:any) : boolean {
+    if (typeof BigInt !== 'function') return false
+    if (typeof x === 'bigint') return true
+
+    return (function () {
+        try {
+            BigInt.prototype.valueOf.call(x)
+            return true
+        } catch (_) {
+            return false
+        }
+    })()
+}
+
+function isNonNegBigInt (x:any) : boolean {
+    return isBigInt(x) && x >= 0n
+}
+
+function isPosBigInt (x:any) : boolean {
+    return isPosBigInt(x) && x > 0n
+}
+
 function isInt (x:any) : boolean {
+    if (isBigInt(x)) return true
     return typeof x === 'number'
         && !isNaN(x)
         && (x | 0) === x
 }
 
 function isNonNegInt (x:any) : boolean {
-    return isInt(x) && x >= 0
+    return isNonNegBigInt(x) || (isInt(x) && x >= 0)
 }
 
 function isPosInt (x:any) : boolean {
-    return isInt(x) && x > 0
+    return isPosBigInt(x) || (isInt(x) && x > 0)
 }
 
 export {
     isInt,
     isNonNegInt,
     isPosInt,
+
+    isBigInt,
+    isNonNegBigInt,
+    isPosBigInt,
 }
