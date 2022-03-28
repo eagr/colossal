1e4611358d66efd2aaaf509021034053a9c90625
eagr <eagr@tutanota.com>
2022-03-28 15:25:03 +0800

    sumFibonacci(n)

--- src/set.ts
+++ src/set.ts
@@ -1,19 +1,6 @@
 import { isNonNegInt, isPosInt } from './util/check'
 import { memoize } from './util/memoize'
 
-/**
- * Calculate the sum of the first `n` positive integers, each to the power of `k`.
- * @param {number} n - the first `n` positive integers
- * @param {number} p - power of the integers, can be either 1, 2, or 3
- */
- function sumOf (n:number, p=1) : number {
-    if (!isPosInt(n)) throw new Error('expect first arg to be a positive integer')
-    if (p !== 1 && p !== 2 && p !== 3) throw new Error('expect second arg to be either 1, 2, or 3')
-    if (p === 1) return n * (1 + n) / 2
-    if (p === 2) return n * (n + 1) * (2 * n + 1) / 6
-    return n * n * (n + 1) * (n + 1) / 4
-}
-
 const fibonacci = (function () {
     const fib = memoize(
         function (n:number) : [number, number] {
@@ -39,8 +26,36 @@ const factorial = memoize(
     }
 )
 
+/**
+ * Calculate the sum of the first `n` positive integers, each to the power of `k`.
+ * @param {number} n - the first `n` positive integers
+ * @param {number} p - power of the integers, can be either 1, 2, or 3
+ */
+ function sumOf (n:number, p=1) : number {
+    if (!isPosInt(n)) throw new Error('expect first arg to be a positive integer')
+    if (p !== 1 && p !== 2 && p !== 3) throw new Error('expect second arg to be either 1, 2, or 3')
+    if (p === 1) return n * (1 + n) / 2
+    if (p === 2) return n * (n + 1) * (2 * n + 1) / 6
+    return n * n * (n + 1) * (n + 1) / 4
+}
+
+/**
+ * F(n)     = F(n+2) - F(n+1)
+ * F(n-1)   = F(n+1) - F(n)
+ * ...
+ * F(1)     = F(3) - F(2)
+ * F(0)     = F(2) - F(1)
+ *
+ * S(n) = F(n) + F(n-1) + ... + F(1) + F(0)
+ *      = F(n+2) - F(1)
+ */
+function sumFibonacci (n:number) : number {
+    return fibonacci(n + 2) - fibonacci(1)
+}
+
 export {
-    sumOf,
     fibonacci,
     factorial,
+    sumOf,
+    sumFibonacci,
 }
