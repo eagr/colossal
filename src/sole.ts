b800174790e95c17d9c7bd9f3c3222d4bf217142
eagr <eagr@tutanota.com>
2022-03-28 16:48:36 +0800

    maxPrimeFactor(x)

--- src/sole.ts
+++ src/sole.ts
@@ -2,6 +2,8 @@ import { isPosInt, isInt } from './util/check'
 import { BigMath } from './util/bigint'
 import { primesUpTo } from './primality'
 
+type Num = number | bigint
+
 function numDivisors (x:number) : number {
     if (!isPosInt(x)) throw new Error('expect a positive integer')
     if (x === 1) return 1
@@ -32,7 +34,7 @@ function numDivisors (x:number) : number {
     return count
 }
 
-function sumDigits (x:number|bigint) : number {
+function sumDigits (x:Num) : number {
     if (!isInt(x)) throw new Error('expect an integer')
 
     let b = BigMath.abs(x)
@@ -44,11 +46,11 @@ function sumDigits (x:number|bigint) : number {
     return Number(s)
 }
 
-function maxProperFactor<T extends number|bigint> (x:T) : T {
+function maxProperFactor<T extends Num> (x:T) : T {
     if (!isInt(x)) throw new Error('expect an integer')
 
-    const isNum = typeof x === 'number'
-    let b = BigMath.abs(x)
+    const isNumber = typeof x === 'number'
+    const b = BigMath.abs(x)
     if (b <= 1n) throw new Error('expect abx(x) to be greater than 1')
 
     let mf = 1n
@@ -64,11 +66,41 @@ function maxProperFactor<T extends number|bigint> (x:T) : T {
         }
     }
 
-    return (isNum ? Number(mf) : mf) as T
+    return (isNumber ? Number(mf) : mf) as T
+}
+
+function maxPrimeFactor<T extends Num> (x:T) : T {
+    if (!isInt(x)) throw new Error('expect an integer')
+
+    const isNumber = typeof x === 'number'
+    const b = BigMath.abs(x)
+    if (b <= 1n) throw new Error('expect abx(x) to be greater than 1')
+
+    let mf = b
+
+    // order matters
+    while (mf % 2n === 0n && mf > 2n) mf /= 2n
+    while (mf % 3n === 0n && mf > 3n) mf /= 3n
+
+    let i = 5n
+    while (i <= BigMath.sqrt(mf)) {
+        if (mf % i === 0n) {
+            mf /= i
+            i = 5n
+        } else if (mf % (i + 2n) === 0n) {
+            mf /= i + 2n
+            i = 5n
+        } else {
+            i += 6n
+        }
+    }
+
+    return (isNumber ? Number(mf) : mf) as T
 }
 
 export {
     numDivisors,
     sumDigits,
     maxProperFactor,
+    maxPrimeFactor,
 }
