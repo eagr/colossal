5b53752d14d066a94cc2cea81a6a8daf09f40a84
eagr <eagr@tutanota.com>
2022-03-29 13:08:34 +0800

    bigint-compat. gcd()

--- src/dual.ts
+++ src/dual.ts
@@ -1,22 +1,5 @@
-import { isInt, isNonNegInt } from './util/check'
-
-function gcd (x:number, y:number) : number {
-    if (!isInt(x) || !isInt(y)) throw new Error('expect integers')
-    if (x === 0 && y === 0) throw new Error('gcd(0, 0) is undefined')
-    if (x === 0) return y
-    if (y === 0) return x
-
-    const l = Math.max(x, y)
-    let s = Math.min(x, y)
-    let r = l % s
-
-    while (r > 0) {
-        const rem = s % r
-        s = r
-        r = rem
-    }
-    return Math.abs(s)
-}
+import { isNonNegInt } from './util/check'
+import { gcd } from './util/arithmetic'
 
 function lcm (x:number, y:number) : number {
     if (!isNonNegInt(x) || !isNonNegInt(y)) throw new Error('expect non-negative integers')
--- src/util/arithmetic.ts
+++ src/util/arithmetic.ts
@@ -1,3 +1,8 @@
+import { isInt } from './check'
+import { BigMath } from './bigint'
+
+type Num = number | bigint
+
 function powMod (b:number, p:number, m:number) : number {
     b %= m
     let res = 1
@@ -9,6 +14,29 @@ function powMod (b:number, p:number, m:number) : number {
     return res
 }
 
+function gcd<T extends Num> (x:T, y:T) : T {
+    if (!isInt(x) || !isInt(y)) throw new Error('expect integers')
+
+    const isNumber = typeof x === 'number' && typeof y === 'number'
+    const bx = BigMath.abs(x)
+    const by = BigMath.abs(y)
+    if (bx === 0n && by === 0n) throw new Error('gcd(0, 0) is undefined')
+    if (bx === 0n) return (isNumber ? Number(by) : by) as T
+    if (by === 0n) return (isNumber ? Number(bx) : bx) as T
+
+    let l = BigMath.max(bx, by)
+    let s = BigMath.min(bx, by)
+    let r = l % s
+
+    while (r > 0n) {
+        const rem = s % r
+        s = r
+        r = rem
+    }
+    return (isNumber ? Number(s) : s) as T
+}
+
 export {
     powMod,
+    gcd,
 }
