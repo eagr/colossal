1cb1d118d720a1d8ac60b7da8569e966543d6ee5
eagr <eagr@tutanota.com>
2022-03-30 12:38:59 +0800

    simplify gcd()

--- src/util/arithmetic.ts
+++ src/util/arithmetic.ts
@@ -18,22 +18,16 @@ function gcd<T extends Num> (x:T, y:T) : T {
     if (!isInt(x) || !isInt(y)) throw new Error('expect integers')
 
     const areNumbers = typeof x === 'number' && typeof y === 'number'
-    const bx = BigMath.abs(x)
-    const by = BigMath.abs(y)
+    let bx = BigMath.abs(x)
+    let by = BigMath.abs(y)
     if (bx === 0n && by === 0n) throw new Error('gcd(0, 0) is undefined')
-    if (bx === 0n) return (areNumbers ? Number(by) : by) as T
-    if (by === 0n) return (areNumbers ? Number(bx) : bx) as T
 
-    let l = BigMath.max(bx, by)
-    let s = BigMath.min(bx, by)
-    let r = l % s
-
-    while (r > 0n) {
-        const rem = s % r
-        s = r
-        r = rem
+    while (true) {
+        if (bx === 0n) return (areNumbers ? Number(by) : by) as T
+        by %= bx
+        if (by === 0n) return (areNumbers ? Number(bx) : bx) as T
+        bx %= by
     }
-    return (areNumbers ? Number(s) : s) as T
 }
 
 function lcm<T extends Num> (x:T, y:T) : T {
