8703bc733753aedb8edbc1f0f286a89468a29ccb
eagr <eagr@tutanota.com>
2022-04-03 16:25:47 +0800

    buffer prime sieve

--- src/primality.ts
+++ src/primality.ts
@@ -16,27 +16,29 @@ function isPrime (x:Num) : boolean {
     return true
 }
 
+let _sieve:boolean[] = []
 function sieveOfEratosthenes (limit:Num) : boolean[] {
     assertRange(isInt(limit) && limit >= 2, 'Expect an integer greater than 1')
-
     const l = Number(limit)
-    const sieve = new Array(l)
-    for (let i = 0; i < l; i += 2) sieve[i] = false
-    if (l > 1) sieve[1] = false
-    if (l > 2) sieve[2] = true
-    for (let i = 3; i < l; i += 2) sieve[i] = true
+    if (l < _sieve.length) return _sieve.slice(0, Number(l))
+
+    _sieve = new Array(l)
+    for (let i = 0; i < l; i += 2) _sieve[i] = false
+    if (l > 1) _sieve[1] = false
+    if (l > 2) _sieve[2] = true
+    for (let i = 3; i < l; i += 2) _sieve[i] = true
 
     const maxOdd = l - 1 - (l % 2)
     const cross = Math.floor(Math.sqrt(maxOdd))
 
     for (let i = 3; i <= cross; i += 2) {
-        if (sieve[i]) {
+        if (_sieve[i]) {
             for (let j = i * i; j <= maxOdd; j += i) {
-                sieve[j] = false
+                _sieve[j] = false
             }
         }
     }
-    return sieve
+    return _sieve.slice(0)
 }
 
 function primes <
