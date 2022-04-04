95fcd44c521fb2dd4e023f6f215f9cb1b2c016cc
eagr <eagr@tutanota.com>
2022-04-04 14:16:35 +0800

    downstream gcd() & lcm()

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -1,28 +1,6 @@
 import type { Num } from './interface'
 import { box } from './util/box'
-import { BigMath } from './util/bigint'
-import { isInt, isPosInt, isZero } from './util/check'
-
-const gcd = box(function (x:Num, y:Num) : bigint {
-    if (isZero(x) && isZero(y)) throw new Error('GCD(0, 0) is undefined')
-    if (!isInt(x) || !isInt(y)) throw new RangeError('Expect integers')
-
-    let bx = BigMath.abs(x)
-    let by = BigMath.abs(y)
-    while (true) {
-        if (bx === 0n) return by
-        by %= bx
-        if (by === 0n) return bx
-        bx %= by
-    }
-})
-
-const lcm = box(function (x:Num, y:Num) : bigint {
-    if (isZero(x) && isZero(y)) return 0n
-    const bx = BigMath.abs(x)
-    const by = BigMath.abs(y)
-    return bx * by / gcd(bx, by)
-})
+import { isPosInt } from './util/check'
 
 const powMod = box(function (b:Num, p:Num, m:Num) : bigint {
     if (!isPosInt(b) || !isPosInt(p) || !isPosInt(m)) throw new RangeError('Expect positive integers')
@@ -42,7 +20,5 @@ const powMod = box(function (b:Num, p:Num, m:Num) : bigint {
 })
 
 export {
-    gcd,
-    lcm,
     powMod,
 }
--- src/dual.ts
+++ src/dual.ts
@@ -1,1 +1,30 @@
-export { gcd, lcm } from './arithmetic'
+import type { Num } from './interface'
+import { box } from './util/box'
+import { BigMath } from './util/bigint'
+import { isInt, isZero } from './util/check'
+
+const gcd = box(function (x:Num, y:Num) : bigint {
+    if (isZero(x) && isZero(y)) throw new Error('GCD(0, 0) is undefined')
+    if (!isInt(x) || !isInt(y)) throw new RangeError('Expect integers')
+
+    let bx = BigMath.abs(x)
+    let by = BigMath.abs(y)
+    while (true) {
+        if (bx === 0n) return by
+        by %= bx
+        if (by === 0n) return bx
+        bx %= by
+    }
+})
+
+const lcm = box(function (x:Num, y:Num) : bigint {
+    if (isZero(x) && isZero(y)) return 0n
+    const bx = BigMath.abs(x)
+    const by = BigMath.abs(y)
+    return bx * by / gcd(bx, by)
+})
+
+export {
+    gcd,
+    lcm,
+}
