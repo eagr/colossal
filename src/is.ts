3c8b66e45d054ec8478907f82212652c48afa8e8
eagr <eagr@tutanota.com>
2022-04-06 10:01:45 +0800

    isPerfect(x)

--- src/is.ts
+++ src/is.ts
@@ -1,15 +1,28 @@
 import type { Num } from './interface'
-import { assertNonNegInt } from './util/check'
+import { isNonNegInt, isPosInt } from './util/check'
 import { BigMath } from './util/bigint'
-import { isPrime } from './_'
+import { isPrime, factors } from './_'
 
-function isSquare (n:Num) : boolean {
-    assertNonNegInt(n)
-    const sqrt = BigMath.sqrt(n)
-    return BigInt(n) === sqrt * sqrt
+function isPerfect (x:Num) : boolean {
+    if (!isPosInt(x)) return false
+
+    const bx = BigInt(x)
+    const fs = factors(bx)
+    let sum = 0n
+    for (let i = 0; i < fs.length - 1; i++) {
+        sum += fs[i]
+    }
+    return sum === bx
+}
+
+function isSquare (x:Num) : boolean {
+    if (!isNonNegInt(x)) return false
+    const sqrt = BigMath.sqrt(x)
+    return BigInt(x) === sqrt * sqrt
 }
 
 export {
+    isPerfect,
     isPrime,
     isSquare,
 }
